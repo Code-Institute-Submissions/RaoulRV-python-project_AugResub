8b57a5afd9f7bc565976dc4e6d7f9a6990e485dd
RaoulRV <raoulradulescu@gmail.com>
2022-03-10 22:51:30 +0000

    Added and defined variables for user, computer imput

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
 import random
 
 from itertools import combinations
+
+# numbers which user is allowed to input
+number_list = [1,2,3,4,5,6,7,8,9]
+
+# list of winning numbers
+winner_list = [
+    [1,2,3],
+    [1,4,7],
+    [1,5,9],
+    [2,5,8],
+    [3,5,7],
+    [3,6,9],
+    [4,5,6],
+    [7,8,9]
+]
+
+nochance_list = []
+
+
+reserved_list = []
+
+# user input list
+user_list = []
+
+# computer input list
+computer_list = []
